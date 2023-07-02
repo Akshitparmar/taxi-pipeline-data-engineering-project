8871e452f34d34363e1d0f7951f81c957197c8b6
Akshit Parmar <72138022+Akshitparmar@users.noreply.github.com>
2023-07-03 00:59:06 +0530

    Update biq_query.sql

--- biq_query.sql
+++ biq_query.sql
@@ -1,4 +1,4 @@
-create or replace table parmar-taxi-proiect.taxi_data.tbl_analvtics as(
+create or replace table 'parmar-taxi-proiect.taxi_data.tbl_analvtics' as(
 SELECT 
 f.trip_id
 f.VendorID,
@@ -21,9 +21,9 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 parmar-taxi-project.taxi_data.fact_table f
-join parmar-taxi-project.taxi_data.datetime_dim d on fdatetime_id-d.datetime_id
+join parmar-taxi-project.taxi_data.datetime_dim d on fdatetime_id=d.datetime_id
 JOIN parmar-taxi-project.taxi_data.passenger_count_dim p ON p.passenger_count_id=f.passenger_count_id
-JOIN parmar-taxi-project.taxi_data.trip_distance_dim t ON t.trip_distance_id-f.trip_distance_id
+JOIN parmar-taxi-project.taxi_data.trip_distance_dim t ON t.trip_distance_id=f.trip_distance_id
 JOIN parmar-taxi-project.taxi_data.rate_code_dim r ON r.rate_code_id=f.rate_code_id
 JOIN parmar-taxi-project.taxi_data.pickup_location_dim pick ON pick.pickup_location_id=f.pickup_location_id
 JOIN parmar-taxi-project.taxi_data.dropoff_location_dim drop ON drop.dropoff_location_id=f.dropoff_location_id
