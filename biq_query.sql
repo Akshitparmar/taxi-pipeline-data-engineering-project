9a7bd371a617569be24b4638bd97a5a39198a46b
Akshit Parmar <72138022+Akshitparmar@users.noreply.github.com>
2023-07-03 01:00:35 +0530

    Update biq_query.sql

--- biq_query.sql
+++ biq_query.sql
@@ -20,12 +20,12 @@ f.tolls_amount,
 f.improvement_surcharge,
 f.total_amount
 FROM 
-parmar-taxi-project.taxi_data.fact_table f
-join parmar-taxi-project.taxi_data.datetime_dim d on fdatetime_id=d.datetime_id
-JOIN parmar-taxi-project.taxi_data.passenger_count_dim p ON p.passenger_count_id=f.passenger_count_id
-JOIN parmar-taxi-project.taxi_data.trip_distance_dim t ON t.trip_distance_id=f.trip_distance_id
-JOIN parmar-taxi-project.taxi_data.rate_code_dim r ON r.rate_code_id=f.rate_code_id
-JOIN parmar-taxi-project.taxi_data.pickup_location_dim pick ON pick.pickup_location_id=f.pickup_location_id
-JOIN parmar-taxi-project.taxi_data.dropoff_location_dim drop ON drop.dropoff_location_id=f.dropoff_location_id
-JOIN parmar-taxi-project.taxi_data..payment_type_dim pay ON pay.payment_type_id=f.payment_type_id)
+'parmar-taxi-project.taxi_data.fact_table' f
+join 'parmar-taxi-project.taxi_data.datetime_dim' d on f.datetime_id=d.datetime_id
+JOIN 'parmar-taxi-project.taxi_data.passenger_count_dim' p ON p.passenger_count_id=f.passenger_count_id
+JOIN 'parmar-taxi-project.taxi_data.trip_distance_dim' t ON t.trip_distance_id=f.trip_distance_id
+JOIN 'parmar-taxi-project.taxi_data.rate_code_dim' r ON r.rate_code_id=f.rate_code_id
+JOIN 'parmar-taxi-project.taxi_data.pickup_location_dim' pick ON pick.pickup_location_id=f.pickup_location_id
+JOIN 'parmar-taxi-project.taxi_data.dropoff_location_dim' drop ON drop.dropoff_location_id=f.dropoff_location_id
+JOIN 'parmar-taxi-project.taxi_data..payment_type_dim' pay ON pay.payment_type_id=f.payment_type_id)
 ;
